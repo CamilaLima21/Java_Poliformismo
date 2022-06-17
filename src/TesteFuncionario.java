f73edbfe99b7303cf4510faec6d067bc52957bb4
Camila <cml.isa.17@gmail.com>
2022-06-17 12:55:45 -0300

    Inicialização do projeto

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+
+		Funcionario func1 = new Funcionario();
+		func1.setNome("Camila Marques");
+		func1.setCpf("12345678900");
+		func1.setSalario(2600.00);
+		
+		System.out.println(func1.getNome());
+		System.out.println(func1.getBonificacao());
+	}
+
+}
