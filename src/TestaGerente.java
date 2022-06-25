de72df6f5a505876a6994a1681f7a25cdfc59d80
Camila <cml.isa.17@gmail.com>
2022-06-25 20:39:27 -0300

    Implementando interfaces

--- src/TestaGerente.java
+++ src/TestaGerente.java
@@ -2,6 +2,9 @@
 public class TestaGerente {
 
 	public static void main(String[] args) {
+		
+		Autenticavel referencia = new Gerente();
+		
 		Gerente g1 = new Gerente();
 		g1.setNome("Marco");
 		g1.setCpf("99999999");
@@ -11,10 +14,10 @@ public class TestaGerente {
 		System.out.println(g1.getCpf());
 		System.out.println(g1.getSalario());
 		
-//		g1.setSenha(1234);
-//		boolean autenticou = g1.autentica(1234);
-//		
-//		System.out.println(autenticou);
+		g1.setSenha(1234);
+		boolean autenticou = g1.autentica(1234);
+		
+		System.out.println(autenticou);
 		
 		System.out.println(g1.getBonificacao());
 
